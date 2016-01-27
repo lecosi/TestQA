365858c186b97eab34346992d5d680226e8b822f
lecosi <leohcollazos@gmail.com>
2016-01-27 14:00:09 -0500

    prueba_jenkins2

--- TestLogin.py
+++ TestLogin.py
@@ -2,30 +2,15 @@ import unittest
 from selenium import webdriver
 
 
-SAUCE_USERNAME = 'lecosi'
-SAUCE_ACCESS_KEY = '42bf6ac2-da98-4022-9d5a-09eb000ecdd0'
+# this is how you set up a test to run on Sauce Labs
+desired_cap = {
+    'platform': "Mac OS X 10.9",
+    'browserName': "chrome",
+    'version': "31",
+}
+driver = webdriver.Remote(
+    command_executor='http://lecosi:42bf6ac2-da98-4022-9d5a-09eb000ecdd0@ondemand.saucelabs.com:80/wd/hub',
+    desired_capabilities=desired_cap)
 
-
-class Selenium2OnSauce(unittest.TestCase):
-
-    def setUp(self):
-        self.driver = webdriver.Remote(
-            desired_capabilities=webdriver.DesiredCapabilities.CHROME,
-            command_executor='http://%s:%s@ondemand.saucelabs.com:80/wd/hub' %
-            (SAUCE_USERNAME, SAUCE_ACCESS_KEY)
-        )
-
-    def test_from_sauce(self):
-        self.driver.get('http://saucelabs.com/test/guinea-pig')
-        self.assertEqual('I am a page title - Sauce Labs', self.driver.title)
-        body = self.driver.find_element_by_css_selector('body')
-        self.assertIn('This page is a Selenium sandbox', body.text)
-
-    def tearDown(self):
-        id = self.driver.session_id
-        print 'Link to your job: https://saucelabs.com/jobs/%s' % id
-        self.driver.quit()
-
-
-if __name__ == '__main__':
-    unittest.main()
+driver.get('https://wiki.saucelabs.com/display/DOCS/Python+Test+Setup+with+Sauce+Labs+Example')
+driver.quit()
