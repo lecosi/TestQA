918f2889dc75573da2570af2c82a8fb87010982b
lecosi <leohcollazos@gmail.com>
2016-02-03 16:25:58 -0500

    Update TestLogin.py
    update test

--- TestLogin.py
+++ TestLogin.py
@@ -1,43 +1,54 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-import time, unittest,copy
-import wd.parallel
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
 
-class parallel(unittest.TestCase):
-    SAUCE_USERNAME = 'lecosi'
-    SAUCE_ACCESS_KEY = '42bf6ac2-da98-4022-9d5a-09eb000ecdd0'
-
+class TestLogin(unittest.TestCase):
     def setUp(self):
 
-        desired_capabilities = []
-
-        browser = copy.copy(webdriver.DesiredCapabilities.FIREFOX)
-        browser['platform'] = 'Windows 8.1'
-        browser['name'] = 'Python Test Express Firefox'
-        browser['tags'] = "Parallel"
-        browser['build'] = "1111"
-        desired_capabilities += [browser]
-
-        browser = copy.copy(webdriver.DesiredCapabilities.CHROME)
-        browser['platform'] = 'Windows 7'
-        browser['name'] = 'Python Test Express Chrome'
-        browser['tags'] = "Parallel"
-        browser['build'] = "1111"
-        desired_capabilities += [browser]
-
-        self.drivers = wd.parallel.Remote(
-        desired_capabilities=desired_capabilities,
-        command_executor="http://" + parallel.SAUCE_USERNAME + ":" + parallel.SAUCE_ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub")
-        print 'Conectando a SauceLabs....'
-
-    @wd.parallel.multiply
-    def test_parallel(self):
-        self.driver.get("https://seguros.comparamejor.com/seguros-para-vehiculos/express/#/consultar-placa")
-        self.driver.find_element_by_id("vehicle_registration").send_keys("yph79c")
-
+        url = 'http://lecosi1:Xeq938dyPSnjXaVHKYpK@hub.browserstack.com:80/wd/hub'
+        self.driver = webdriver.Remote(command_executor=url,
+        desired_capabilities=DesiredCapabilities.CHROME)
+        self.driver.implicitly_wait(5)
+        self.base_url = "https://seguros.comparamejor.com/crm"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_login(self):
+        driver = self.driver
+        driver.get(self.base_url + "/usuarios/login/")
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("leonardo@comparamejor.com")
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("leonardocollazos1234")
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
         self.driver.quit()
-        print 'Ejecución terminada!'
+        self.assertEqual([], self.verificationErrors)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
